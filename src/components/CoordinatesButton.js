1a95fb32b63c38d2c568093813828695e9da8d5f
kelly r maass <kmaass77627@gmail.com>
2021-02-21 19:09:09 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,13 @@
 // Code CoordinatesButton Component Here
+import React from 'react'
+
+export default class CoordinatesButton extends React.Component {
+
+render() {
+
+  return(
+<div></div>
+  )
+}
+
+}
