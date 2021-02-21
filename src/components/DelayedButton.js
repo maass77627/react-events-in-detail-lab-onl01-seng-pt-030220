7c5aa7049ba253fd1a4eeb4746e3ed7d2c10b9d9
kelly r maass <kmaass77627@gmail.com>
2021-02-21 19:31:46 +0000

    Automatically backed up by Learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -5,9 +5,11 @@ export default class DelayedButton extends React.Component {
 
   handleClick = (event) => {
     event.persist()
-    setTimeout(this.props.onDelayedClick(event), this.props.delay)
-  }
+  //   setTimeout(this.props.onDelayedClick(event), this.props.delay)
+  // }
 
+  setTimeout(() => { this.props.onDelayedClick(event); }, this.props.delay);
+}
 render() {
 
   return(
